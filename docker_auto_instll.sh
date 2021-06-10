08aa4af4a5348cc2cc3403f48abc815753aa1788
FCG <admin@iotlet.cn>
2021-06-10 10:58:11 +0800

    优化

--- docker_auto_instll.sh
+++ docker_auto_instll.sh
@@ -9,15 +9,8 @@ oneinstack_dir=$(dirname "`readlink -f $0`")
 pushd ${oneinstack_dir} > /dev/null
 . ./include/check_os.sh
 
-if [ "${PM}" == 'apt-get' ];then
-  apt-get -y update
-  apt-get -y remove docker docker-engine docker.io containerd runc 
-  apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu ${Code_Name} stable"
-  apt-get -y update
-  apt-get -y install docker-ce docker-ce-cli containerd.io
 
+Docker_Config(){
   # 普通用户使用docker
   usermod -aG docker $USER
 
@@ -32,8 +25,26 @@ if [ "${PM}" == 'apt-get' ];then
 
   # docker-compose
   curl -L "https://gitee.com/AsuKa1989/ohmyshell/attach_files/732673/download/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose;
-  chmod +x /usr/local/bin/docker-compose;
+  chmod 777 /usr/local/bin/docker-compose;
 
-  systemctl restart docker; docker version; docker-compose version
-  echo "docker安装完成;)"
-fi
+  echo "===this is Cutting line==="
+  docker -v; docker-compose -v
+  echo "docker install succeed ;)"
+}
+
+
+case "${LikeOS}" in
+  "CentOS")
+    echo "centos 支持"
+    ;;
+  "Ubuntu")
+    apt-get -y update
+    apt-get -y remove docker docker-engine docker.io containerd runc 
+    apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu ${Code_Name} stable"
+    apt-get -y update
+    apt-get -y install docker-ce docker-ce-cli containerd.io
+    Docker_Config
+    ;;
+esac
