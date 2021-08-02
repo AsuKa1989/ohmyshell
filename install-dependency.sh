9f433ed3ab89d594d8b5adc987631cbe26f528f2
FCG <admin@iotlet.cn>
2021-08-02 12:10:24 +0800

    自动安装oh-my-zsh

--- install-dependency.sh
+++ install-dependency.sh
@@ -25,3 +25,8 @@ git \
 zsh \
 dstat \
 tree
+
+git clone -b fcg-branch --depth=1 https://gitee.com/AsuKa1989/oh-my-zsh.git ../.oh-my-zsh && cd ../.oh-my-zsh
+cp ./my-custom/plugins/zsh-syntax-highlighting.tgz ./custom/plugins && tar -xzvf ./custom/plugins/zsh-syntax-highlighting.tgz -C ./custom/plugins/
+
+chsh -s /bin/zsh
